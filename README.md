d3c5c6d9db355455fdef3517eca81bb4668085f0
Ryan Oliveira <contato.ryanolivr@outlook.com>
2022-10-19 22:48:36 -0300

    FirstCommit

---
+++ README.md
@@ -0,0 +1,41 @@
+# NodeJS Simple API
+
+<p align="center">
+  <a href="#-tecnologias">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projects</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+
+
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- JavaScript
+- NodeJS
+- Express
+
+
+
+## 💻 Projeto
+
+Esse projeto foi construído para servir de apoio no estudo de consumo de APIs REST
+
+
+
+## Como utilizar?
+
+1. Baixe esse repositório e com o seu o terminal, entre no diretório
+
+2. Rode o comando `npm install` para instalar as dependências
+
+3. E pra iniciar a aplicação, rode o comando `npm start` 
+
+## Rotas
+1. Buscar todos os usuarios - get "/api"
+2. Buscar apenas um usuario - get "/ap/:id"
+3. Criar um novo usuario - post "/api/createperson"
+4. Atualizar um usuario - put "/api/update/:id"
+5. Atualizar uma informação - patch "/api/update/:id/:patch"
+6. Deletar um usuario - delete "/api/delete/:id"
---
+++ index.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.listen(5500, () => console.log("Rodando na porta 5500"));
+
+app.use(cors());
+
+app.use(express.json());
+
+let users = [
+  {
+    id: 1,
+    name: "Ryan Oliveira",
+    avatar: "https://avatars.githubusercontent.com/u/66662333?v=4",
+    city: "Belem, Pará",
+  },
+];
+
+app.route("/api").get((req, res) =>
+  res.json({
+    users,
+  })
+);
+
+app.route("/api/:id").get((req, res) => {
+  const userId = req.params.id;
+
+  const user = users.find((user) => Number(user.id) === Number(userId));
+
+  if (!user) {
+    return res.status(404).json("User nor found!");
+  }
+
+  res.json(user);
+});
+
+app.route("/api/createperson").post((req, res) => {
+  const lastId = users[users.length - 1].id;
+
+  users.push({
+    id: lastId + 1,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    city: req.body.city,
+  });
+
+  res.status(201).json("Saved user");
+});
+
+app.route("/api/update/:id").put((req, res) => {
+  const userId = req.params.id;
+
+  const user = users.find((user) => Number(user.id) === Number(userId));
+
+  if (!user) {
+    return res.status(404).json("User nor found!");
+  }
+
+  const updatedUser = {
+    ...user,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    city: req.body.city,
+  };
+
+  users = users.map((user) => {
+    if (Number(user.id) === Number(userId)) {
+      user = updatedUser;
+    }
+    return user;
+  });
+
+  res.status(201).json("Updated user");
+});
+
+app.route("/api/update/:id/:patch").patch((req, res) => {
+  const userId = req.params.id;
+  let patch = req.params.patch;
+
+  const user = users.find((user) => Number(user.id) === Number(userId));
+
+  if (!user) {
+    return res.status(404).json("User not found!");
+  }
+
+  let updatedUser
+
+  switch (patch) {
+    case "name":
+      updatedUser = {
+        ...user,
+        name: req.body.name,
+      };
+      break
+    case "avatar":
+       updatedUser = {
+        ...user,
+        avatar: req.body.avatar,
+      };
+      break
+      case "city":
+        updatedUser = {
+          ...user,
+          city: req.body.city,
+        };
+        break
+      default:
+        return res.status(404).json("parameter not found!")
+  }
+
+  users = users.map((user) => {
+    if (Number(user.id) === Number(userId)) {
+      user = updatedUser;
+    }
+    return user;
+  });
+
+  res.status(201).json("Updated user");
+});
+
+app.route("/api/delete/:id").delete((req, res) => {
+  const userId = req.params.id;
+
+  users = users.filter((user) => Number(user.id) !== Number(userId));
+
+  res.status(202).json("Deleted User");
+});
